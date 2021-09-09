99d9e32b489f96d8930576bcffdc5e87e9605f4a
akamitchu <44710265+akamitchu@users.noreply.github.com>
2021-09-09 19:18:55 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'MTSI',
+    author: 'Nilesh Mahajan',
+    description: 'Mahajan Technology Solutions Inc',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
